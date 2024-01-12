872befdea67acf0f6fff317989bba007fe2bbd1c
JinYoung <xmfkdnak98@gmail.com>
2024-01-12 22:06:14 +0900

    깃 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App() {
+  const url = 'https://kecommerce.shop/api';
+
+    axios.get(url + '/categories/TOP')
+    .then(function (response) {
+      // 성공 핸들링
+      console.log(response);
+    })
+    .catch(function (error) {
+      // 에러 핸들링
+      console.log(error);
+    })
+    .finally(function () {
+      // 항상 실행되는 영역
+    });
+
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      그냥 이커머스 
     </div>
   );
 }
