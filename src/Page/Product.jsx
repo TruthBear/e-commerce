93ea9b2b2d86b62d77a298c566d8fb788ff42e75
JinYoung <xmfkdnak98@gmail.com>
2024-01-17 21:40:01 +0900

    product page 수정

--- src/Page/Product.jsx
+++ src/Page/Product.jsx
@@ -1,35 +1,32 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 
 export default function Product() {
   const productId = useParams().id;
   const [product, setProduct] = useState();
+  
   useEffect(()=>{
     const url = `https://kecommerce.shop/api/product/${productId}`
     axios.get(url)
     .then(function (response) {
-      console.log(response.data.entity == false)
       setProduct(response.data.entity)
+      console.log(response.data.entity)
     })
     .catch(function (error) {
       console.log(error);
     })
-  },[productId])
+  },[])
   return ( 
-    <div className='w-[1200px] flex justify-center'>
-      {
-          
-      }        
-
-      {/* <div>
-        <img src="https://placehold.co/600" alt="상품이름" />
+    <div className='w-[1200px] flex flex-col items-center'>     
+      <div className='w-[1000px] flex justify-between bg-slate-100'>
+        <div>
+          <img src="https://placehold.co/400" alt="상품이름" />
+        </div>
+        <div>
+          <p>상품 제목</p>
+        </div>
       </div>
-      <div>
-        <p>상품 제목</p>
-        <p>상품 브랜드</p>
-        <p>상품가격</p>
-      </div> */}
     </div>
   )
 }
--- src/Page/Search.jsx
+++ src/Page/Search.jsx
@@ -7,17 +7,8 @@ export default function Search() {
   const url = new URLSearchParams(location.search)
   console.log(url.get("name"))
 
-  useEffect(()=>{
-    const url = `https://kecommerce.shop/api/categories/${category}/detail?page=${page}`
-    axios.get(url)
-    .then(function (response) {
-      setProducts(response.data.entity.products)
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-  },[])
+  
   return (
-    <div>Search</div>
+    <div>전체 검색은 서비스 종료다!</div>
   )
 }
